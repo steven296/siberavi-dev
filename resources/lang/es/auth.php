a2a6759d33a6f01aff7396057594ca0f41308425
steven bermudez <stevenbm296@hotmail.com>
2020-12-25 21:21:02 -0500

    lang-spanish

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'Datos erróneos. Por favor, verifique sus credenciales.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
